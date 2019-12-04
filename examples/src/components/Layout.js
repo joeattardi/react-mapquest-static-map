a432e023f023c7fbe54fc16f7a3bb03bfa381e8f
Joe Attardi <joe@attardi.net>
2019-12-03 22:47:45 -0500

    Add prismjs for syntax highlighting in examples

--- examples/src/components/Layout.js
+++ examples/src/components/Layout.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import Prism from 'prismjs';
+import 'prismjs/themes/prism.css';
+import 'prismjs/components/prism-jsx.min';
 
 import Header from './Header';
 import Sidebar from './Sidebar';
@@ -6,6 +10,10 @@ import Sidebar from './Sidebar';
 import styles from './Layout.module.scss';
 
 export default function Layout({ children }) {
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
+
   return (
     <div id={styles.siteContainer}>
       <Header />
--- examples/src/components/SourceFile.js
+++ examples/src/components/SourceFile.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function SourceFile({ src }) {
   return (
     <pre>
-      <code dangerouslySetInnerHTML={{ __html: escape(src) }} />
+      <code className="language-jsx" dangerouslySetInnerHTML={{ __html: escape(src) }} />
     </pre>
   );
 }
